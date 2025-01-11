660eb7109cc813d930ecd32f9f109ef58d5129ff
TywrapStudios <tywrap.studio@gmail.com>
2025-01-12 00:41:55 +0100

    Add more Assets

--- src/main/java/org/tywrapstudios/constructra/util/datagen/ConstructraDataGenerator.java
+++ src/main/java/org/tywrapstudios/constructra/util/datagen/ConstructraDataGenerator.java
@@ -7,6 +7,7 @@ public class ConstructraDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
 		pack.addProvider(ModelGeneration::new);
 		pack.addProvider(TagGeneration.IItems::new);
 		pack.addProvider(LangGeneration::new);
